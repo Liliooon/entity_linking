3ee20803d23d65076faa49feac50f806affe6d53
Liliooon <liliooonk@gmail.com>
2021-12-23 01:53:51 +0300

    try to implement bootstrap

--- web/main.py
+++ web/main.py
@@ -3,13 +3,15 @@ import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    firstname = request.args.get('firstname', '')
-    res = requests.post('http://api:8000/people/', data={'firstname': firstname})
-    print(f'!!!!!!! {res}', flush=True)
-    items = requests.get('http://api:8000/people/').json().get('_items', [])
-    return render_template('index.html.j2', firstname=firstname, items=items)
+@app.route('/')
+def index():
+    return render_template('index.html.j2')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['input_text']
+    processed_text = text.upper()
+    return render_template('index.html.j2', output_text=processed_text)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
